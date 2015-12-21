1dc83f77ff0dc369d996d2d7eed8fdab94f030e7
amashigeseiji <tenjuu99@gmail.com>
2015-12-22 03:14:33 +0900

    対戦相手の指し手をシミュレーションする(refs #22)

--- model/ai.rb
+++ model/ai.rb
@@ -3,15 +3,34 @@ class AI
     @board_id = board_id
   end
 
+  # 相手の指し手の全体を評価して、
+  # 相手の点数が一番低い手を選択する
+  # 自分の手の良し悪し（枚数が何枚増えるか、角がとれるか）は
+  # 現状で考慮していない
   def move
     moves = board.moves
     return nil if moves.empty?
 
     evaluated = {}
     moves.each do |index, reversibles|
-      evaluated[index] = evaluate(cell(index), reversibles)
+      evaluated[index] = evaluate_opponent(index)
     end
-    cell(evaluated.max_by { |_, item| item }[0])
+    cell(evaluated.min_by { |_, item| item }.first)
+  end
+
+  def simulate(cell_index)
+    simulator = Simulator.new(@board_id)
+    simulator.move_exec cell_index
+    simulator
+  end
+
+  def evaluate_opponent(cell_index)
+    simulator = simulate(cell_index)
+    evaluated = 0
+    simulator.moves.each do |index, cells|
+      evaluated += evaluate(simulator.cells[index], cells)
+    end
+    evaluated
   end
 
   private
