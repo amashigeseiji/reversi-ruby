b3eff848ef98d26fe607f9827d5f57425894761c
amashigeseiji <tenjuu99@gmail.com>
2016-01-01 03:58:56 +0900

    実装を抽象化しておく

--- model/strategy/alpha_beta.rb
+++ model/strategy/alpha_beta.rb
@@ -2,36 +2,28 @@ module Strategy
   class AlphaBeta < AbstractStrategy
     def evaluate(game)
       @search_level = 3
-      alpha_beta(game, @search_level, -Float::INFINITY, Float::INFINITY)
+      alpha_beta(game, @search_level, true, -Float::INFINITY, Float::INFINITY)
     end
 
-    def alpha_beta(game, depth, alpha, beta)
+    def alpha_beta(game, depth, flag, alpha, beta)
       return score(game) if depth == 0 || game.ended? || game.moves.empty?
 
       value = 0
       best_index = nil
 
       game.moves.each do |index, move|
-        move.simulate do |next_game|
+        move.simulate do |node|
           # 次の盤面の評価点
-          value = alpha_beta(next_game, depth - 1, alpha, beta)
+          value = alpha_beta(node, depth - 1, !flag, alpha, beta)
 
-          if game.turn == :white
-            if value > alpha
-              alpha = value
-              best_index = index
-              if (alpha >= beta)
-                return alpha
-              end
-            end
-          else
-            if value < beta
-              beta = value
-              best_index = index
-              if (alpha >= beta)
-                return beta
-              end
-            end
+          if flag && value > alpha
+            alpha = value
+            best_index = index
+            return alpha if alpha >= beta
+          elsif !flag && value < beta
+            beta = value
+            best_index = index
+            return beta if alpha >= beta
           end
         end
       end
@@ -39,7 +31,7 @@ module Strategy
       # ルートノードの場合はセルの位置を返す
       # (それ以外は再帰処理)
       return best_index if depth == @search_level
-      return game.turn == :white ? alpha : beta
+      return flag ? alpha : beta
     end
 
     def score(game)
